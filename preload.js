c6265c7a101f451077725f3022b2f6b726fd0ba3
ABDUL BASIT <absh5506@gmail.com>
2026-07-15 06:25:47 -0700

    security: narrow the Electron preload bridge

--- preload.js
+++ preload.js
@@ -1,8 +1,18 @@
+'use strict';
+
 const { contextBridge, ipcRenderer } = require('electron');
 
-contextBridge.exposeInMainWorld('deskRTC', {
-  startAgent: (hostId, serverUrl) => ipcRenderer.invoke('start-agent', { hostId, serverUrl }),
+contextBridge.exposeInMainWorld('deskRTC', Object.freeze({
+  startAgent: (hostId, agentToken) => ipcRenderer.invoke('start-agent', { hostId, agentToken }),
   stopAgent: () => ipcRenderer.invoke('stop-agent'),
-  onAgentLog: (callback) => ipcRenderer.on('agent-log', (_event, text) => callback(String(text))),
-  onAgentExit: (callback) => ipcRenderer.on('agent-exited', (_event, data) => callback(data))
-});
+  onAgentLog: (callback) => {
+    const listener = (_event, text) => callback(String(text));
+    ipcRenderer.on('agent-log', listener);
+    return () => ipcRenderer.removeListener('agent-log', listener);
+  },
+  onAgentExit: (callback) => {
+    const listener = (_event, data) => callback(Object.freeze({ code: data?.code ?? null }));
+    ipcRenderer.on('agent-exited', listener);
+    return () => ipcRenderer.removeListener('agent-exited', listener);
+  },
+}));
